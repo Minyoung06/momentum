ea092a2710bff15fdb81d6c6feb51e0057856afa
Minyoung06 <monica_y@naver.com>
2019-01-11 01:11:32 +0900

    1-8

---
+++ 1장/index.js
@@ -0,0 +1,20 @@
+/*alert('Im Working. Im JS. iM beautiful. Im worth it');
+console.log('Im Working. Im JS. iM beautiful. Im worth it');*/
+
+/*let a = 221;
+let b = a - 5;
+a = 4
+console.log(b, a);
+*/
+
+/*
+//String
+const what = "Nicolas";//텍스트 저장,이모티콘도 저장가능함,""안에 숫자를 넣어도 텍스트임
+console.log(what);
+//boolean
+const what = true; false 0/true 1
+//float
+const what = 12.15
+ */
+
+ 
