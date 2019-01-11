7fc4ae553f92fc60c5a5997a6887da7bca5a62e3
Minyoung06 <monica_y@naver.com>
2019-01-12 01:58:14 +0900

    2-7

--- 2장/Dom_Function.js
+++ 2장/Dom_Function.js
@@ -3,15 +3,7 @@ title.innerHTML="Hi! Form JS";
 title.style.color='red';
 document.title='I owe you now';*/
 
-/*const title = document.querySelector("#title");
-
-function handleClick(){
-    title.style.color="blue";
-}
-
-title.addEventListener("click", handleClick);//괄호를 붙이면 함수가 바로 호출됨*/
-
-const age = prompt("How old are you");
+/*const age = prompt("How old are you");
 
 if(age >= 18 && age <= 21){
     console.log("you can drink but you should not");
@@ -19,4 +11,23 @@ if(age >= 18 && age <= 21){
     console.log("go ahead");
 }else{
     console.log("you can't");
+}*/
+
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked";
+
+function handleClick() {
+    const currentClass = title.className;
+    if(currentClass !== CLICKED_CLASS){
+        title.className = CLICKED_CLASS;
+    }else{
+        title.className = "";
+    }
 }
+
+function init(){
+    title.addEventListener("click", handleClick);//괄호를 붙이면 함수가 바로 호출됨
+}
+init();
+
